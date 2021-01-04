2dfe21b61c25af903b4ef96dde5851bb4ca33a99
wdong20 <wdong20@stuy.edu>
2021-01-04 12:14:34 -0500

    Setup for part I

---
+++ PigLatin.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class PigLatin {
+    public static void main(String[] aaa) {
+        
+    }
+
+    public static String pigLatinSimple(String s) {
+        return "";
+    }
+}
