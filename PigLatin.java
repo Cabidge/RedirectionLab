acf06d03efbb847989cbd6128b328b0a3eb4bf8b
wdong20 <wdong20@stuy.edu>
2021-01-05 11:49:58 -0500

    Completed and Tested part 1 (pigLatinSimple)

--- PigLatin.java
+++ PigLatin.java
@@ -1,10 +1,32 @@
 import java.util.*;
+
 public class PigLatin {
     public static void main(String[] aaa) {
+        System.out.print(PigLatin.pigLatinSimple("mock"));
+        System.out.println(" = ockmay?");
         
+        System.out.print(PigLatin.pigLatinSimple("pie"));
+        System.out.println(" = iepay?");
+
+        System.out.print(PigLatin.pigLatinSimple("david"));
+        System.out.println(" = avidday?");
+
+        System.out.print(PigLatin.pigLatinSimple("aaron"));
+        System.out.println(" = aaronhay?");
     }
 
     public static String pigLatinSimple(String s) {
-        return "";
+        s = s.toLowerCase();
+
+        char firstChar = s.charAt(0);
+        if (PigLatin.lowerIsVowel(firstChar)) {
+            return s + "hay";
+        } else {
+            return s.substring(1) + firstChar + "ay";
+        }
+    }
+    
+    public static boolean lowerIsVowel(char c) {
+        return "aeiou".indexOf(c) != -1;
     }
 }
